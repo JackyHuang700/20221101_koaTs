972f7567ede2f255de163407419fb60accc1f63c
Jacky <jacky8595125@gmail.com>
2022-11-02 09:02:05 +0800

    ..

---
+++ dist/app/config/env/api.js
@@ -0,0 +1,28 @@
+'use strict';
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    redis_common: {
+        port: 6379,
+        host: '127.0.0.1',
+        password: '123456',
+    },
+    db_common: {
+        host: '127.0.0.1',
+        port: 3306,
+        dbname: 'blog_api',
+        username: 'root',
+        password: 'root',
+        dialect: 'mysql',
+        logging: true,
+        benchmark: true,
+        define: {
+            timestamps: true,
+            freezeTableName: true,
+        },
+        pool: {
+            max: 200,
+            min: 0,
+            idle: 10000,
+        },
+    },
+};
